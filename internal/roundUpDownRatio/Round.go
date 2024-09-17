dea5fa9316251eb65adac040d6180faa8c98d707
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-09-17 23:33:08 +0900

    Add round up/down ratio package and functions

---
+++ internal/roundUpDownRatio/Round.go
@@ -0,0 +1,20 @@
+package roundUpDownRatio
+
+import "github.com/shimauma0312/loto7-promise/internal/result"
+
+var results = result.GetResult(100)
+
+func GetRoundRatio() {
+
+}
+
+func Ratio() {
+
+}
+
+func round(num int) bool {
+	if num%2 == 0 {
+		return false
+	}
+	return true
+}
