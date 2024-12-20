28ae7b696791bf3ab5c68150974264d907a241ee
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-12-20 15:21:30 +0900

    Add logic to adjust number for CSV retrieval based on the day of the week

--- internal/result/lotoCsv.go
+++ internal/result/lotoCsv.go
@@ -7,12 +7,19 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/text/encoding/japanese"
 	"golang.org/x/text/transform"
 )
 
 func GetCsv(num int) ([]string, error) {
+	// Check if today is Friday
+	today := time.Now()
+	if today.Weekday() == time.Friday {
+		num -= 1
+	}
+
 	url := "https://www.mizuhobank.co.jp/retail/takarakuji/loto/loto7/csv/A1030" + strconv.Itoa(num) + ".CSV"
 	return Parse(url)
 }
